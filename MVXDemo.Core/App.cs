d5b15c3964087eadf36a62cd199804f9435ca0be
Callum Edward Arnold <callumarnold@hotmail.com>
2020-10-11 20:26:47 +0100

    Add project files.

---
+++ MVXDemo.Core/App.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.ViewModels;
+using MVXDemo.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVXDemo.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ MVXDemo.Core/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVXDemo.Core.ViewModels
+{
+    public class HomeViewModel : MvxViewModel
+    {
+
+    }
+}
